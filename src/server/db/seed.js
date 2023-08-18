ceaa1625bbb2cbea55226274011cef74ba3d284d
cmccollum325 <cmccollum325@gmail.com>
2023-08-18 13:27:59 -0500

    createProduct function

--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -592,6 +592,7 @@ const seedDatabse = async () => {
         await dropTables();
         await createTables();
         await insertUsers();
+        await insertProducts();
     }
     catch (err) {
         throw err;
