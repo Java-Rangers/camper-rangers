87622dc3e8eb30aa23c70ddf8a2b9921bc27ddb6
AzuraLulamoon <133188559+AzuraLulamoon@users.noreply.github.com>
2023-08-21 10:18:31 -0500

    final for order items branch

---
+++ src/server/db/orderItems.js
@@ -0,0 +1,8 @@
+const createOrderItems = async (orderId, productId, quantity, createdAt, modifiedAt) => {
+    try {
+        const { rows: [orderItems] }
+    } catch(error) {
+        console.error('error creating order items...', error)
+        throw error;
+    }
+} 
