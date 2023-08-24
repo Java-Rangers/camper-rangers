dcb31234f49a931dcdd5e391b19c374384cf5a79
Benjamin McMahon <parachutehappens@gmail.com>
2023-08-24 11:59:18 -0700

    checkout cart feature started

--- src/server/api/cart.js
+++ src/server/api/cart.js
@@ -2,17 +2,30 @@ const express = require('express')
 const cartRouter = express.Router();
 
 const {
-    getCartByUser
+    getCartByUser,
+    checkoutCart
 } = require('../db/cart')
 
 cartRouter.get('/user/:id', async (req, res, next) => {
     try {
         const cart = await getCartByUser(req.params.id);
+
+        // sends an array of orderItem objects
         res.send ({cart})
     } catch(err) {
         console.error('Error while getting cart by user', err);
         throw err;
     }
+})
+
+cartRouter.patch('/user/:id', async (req, res, next) => {
+  try{
+    const cart = await checkoutCart(req.params.id);
+    res.send({cart});
+  }catch(err){
+    console.log('Error during cart checkout of user id: ', req.params.id, err);
+    next(err);
+  }
 })
 
 module.exports = cartRouter;
--- src/server/db/cart.js
+++ src/server/db/cart.js
@@ -1,6 +1,13 @@
+/*
+get cart by user
+add orderItem to order
+(stretch goal) update order to fullfilled
+*/
+
 const db = require('./client')
 const { getOrderItemsByOrder } = require('./orderItems')
 
+// gets the users cart and returns array of orderItems from cart
 const getCartByUser = async(id) => {
   console.log(`---Getting cart by user id: ${id}`)
   try{
@@ -12,12 +19,15 @@ const getCartByUser = async(id) => {
     if (cart.rows.length === 0){
       console.log(`---No unfullfilled orders for user id: ${id}`)
       return;
-    }
+    }   
 
-    // grabs the 
+    // grabs the order id of the unfullfilled order of the user, passes that id to getOrderItemsByOrder
+    // potential bug here if user has multiple unfullfilled orders (a state that shouldn't be allowed), it will only return the first order if this happens
     const cartOrder = await cart.rows[0].id
     const userCart = await getOrderItemsByOrder(cartOrder)
 
+    // returns an array of orderItem objects
+    console.log('---Received cart of user id: ', id, ' ', userCart)
     return userCart
   }catch(err){
     console.error('Error while getting cart by user id', err);
@@ -25,6 +35,27 @@ const getCartByUser = async(id) => {
   }
 }
 
+// toggles cart fullfilled status to true
+const checkoutCart = async (id) => {
+  try{
+    const { rows } = await db.query(`
+    UPDATE orders SET fullfilled=true
+    WHERE "userID"=$1 AND fullfilled=false
+    RETURNING *;`, [id])
+
+    if (rows.length === 0){
+      console.log('---Order not updated')
+      return null;
+    }
+
+    return rows;
+  }catch(err){
+    console.log('Error during cart checkout of user id: ', id,  err);
+    throw err
+  }
+}
+
 module.exports = {
-  getCartByUser
+  getCartByUser,
+  checkoutCart
 }
