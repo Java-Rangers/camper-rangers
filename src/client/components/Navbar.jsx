da31c2ef50dbf5d3f71653553d99cbec65882887
AzuraLulamoon <gabrielreffnercr@gmail.com>
2023-08-25 12:58:01 -0500

    Added additional page plus MUI added Navbar with MUI formatting for use with register user

---
+++ src/client/components/Navbar.jsx
@@ -0,0 +1,28 @@
+// import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Container, Paper, Box, Typography } from '@mui/material'
+
+export default function NavBar() {
+    // const [token, setToken] = useState(localStorage.getItem('token'));
+
+    // useEffect(() => {
+    //     const storedToken = localStorage.getItem('token');
+    //     setToken(storedToken);
+    // }, []);
+
+    return (
+        <Container>
+            <Paper elevation={10}>
+                    <Typography className='navigation' textAlign={'center'}>
+                        <nav>
+                            <Typography variant='h2'>Java Campers</Typography>
+                            <Typography variant='ul'>
+                                    <Typography variant='li' margin={2}><Link to='/products'>Home</Link></Typography>
+                                    <Typography variant='li' margin={2}><Link to='/registerUser'>Register</Link></Typography>
+                            </Typography>
+                        </nav>
+                    </Typography>
+            </Paper>
+        </Container>
+    )    
+}
