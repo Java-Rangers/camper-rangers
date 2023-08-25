13a47978429d42923dcdaf615dcba5ff414c87e9
Benjamin McMahon <parachutehappens@gmail.com>
2023-08-25 08:51:29 -0700

    add items to order complete

--- src/server/db/orderItems.js
+++ src/server/db/orderItems.js
@@ -1,6 +1,9 @@
-const db = require('./client')
-const { getProductById } = require('./products')
+/*
+add products to orderItem
+remove products from orderItem
+*/
 
+const db = require('./client')
 
 const getOrderItemsByOrder = async (id) => {
   try{
@@ -14,42 +17,34 @@ const getOrderItemsByOrder = async (id) => {
   }
 }
 
-const createOrderItem = async ({orderID, productID, quantity}) => {
+const createOrderItem = async ({orderId, productId, quantity}) => {
     try {
-        const { rows: [orderItems] } = await db.query(`
+        const { rows: [orderItem] } = await db.query(`
             INSERT INTO "orderItems"("orderId", "productId", quantity)
             VALUES ($1, $2, $3)
-            RETURNING *
-        `, [orderID, productID, quantity])
-        return orderItems
+            RETURNING *;
+        `, [orderId, productId, quantity])
+        console.log(orderItem)
+        return orderItem
     } catch(error) {
         console.error('error creating order items...', error)
         throw error;
     }
 }
 
-const getAllOrderItems = async() => {
-    try {
-        const { rows: [productId] } = await client.query(`
-            SELECT id
-            FROM "orderItems";
-        `);
-
-        const products = await Promise.all(productId.map(
-            product => getProductById(product.id)
-        ))
-        
-        
-        return products
-
-    } catch(error) {
-        console.error('error getting all order items...',error)
-    }
+const addItemToOrder = async (orderItemId, orderId) => {
+  console.log(`---Adding orderItem ${orderItemId} to order ${orderId}`)
+  try{
+    const order = await db.query(`
+      UPDATE "orderItems" SET "orderId"=$1
+      WHERE id=$2 RETURNING *;`, [orderId, orderItemId])
+      console.log(order)
+      return order
+  }catch(err){
+    console.error(`Error adding orderItem id ${orderItemId}, to order id: ${orderId}`)
+    throw err
+  }
 }
-
-
-
-// const createOrderItem = async()
 
 const editOrderItem = async ({id, orderID, productID, quantity, modifiedAt}) => {
     try {
@@ -66,11 +61,10 @@ const editOrderItem = async ({id, orderID, productID, quantity, modifiedAt}) => 
     }
 }
 
-// const deleteCartItem = async(order)
 
 module.exports = {
     createOrderItem,
-    getAllOrderItems,
     getOrderItemsByOrder,
-    editOrderItem
+    editOrderItem,
+    addItemToOrder
 }
--- src/server/db/orders.js
+++ src/server/db/orders.js
@@ -15,37 +15,47 @@ POST /api/orders
 PATCH /api/orders/id
   ---edits order by id
   ---adding/removing orderItems from order
+
+add orderItem to order
+remove orderItem from order
+figure out when an order is created
 */
 
 const db = require('./client')
 
 const getAllOrders = async () => {
+  console.log('---Getting all orders')
   try{
     const orders = await db.query(`SELECT * FROM orders`);
     return orders.rows;
   }catch(err){
+    console.error('Error getting all orders', err)
     throw err
   }
 }
 
 const getOrder = async (id) => {
+  console.log(`---Getting order by id: ${id}`)
   try{
     const { rows: [order] } = await db.query(`
     SELECT * FROM orders
     WHERE id=$1`, [id]);
     return order;
   }catch(err){
+    console.error(`Error getting order by id: ${id}`, err)
     throw err
   }
 }
 
 const getOrderByUser = async (id) => {
+  console.log(`Getting order by user id: ${id}`)
   try{
     const { rows: [order] } = await db.query(`
     SELECT * FROM orders
     WHERE "userID"=$1`, [id]);
     return order;
   }catch(err){
+    console.error(`Error getting order by user id: ${id}`, err)
     throw err
   }
 }
@@ -59,31 +69,14 @@ const createOrder = async ({userID, total, fullfilled}) => {
         `, [userID, total, fullfilled])
         return orders
     } catch(error) {
-        console.error('error creating orders...', error)
+        console.error('Error creating order', error)
         throw error;
     }
 } 
-
-const getCart = async(userId) => {
-    try {
-        const { rows: [order] } = await db.query(`
-            SELECT *
-            FROM orders
-            WHERE fullfilled = false AND "userID" = $1
-        `, [userId])
-        
-        return order;
-       
-
-    } catch(error) {
-        console.error('error getting cart...',error)
-    }
-}
 
 module.exports = {
   getAllOrders,
   getOrder,
   getOrderByUser,
-  createOrder,
-  getCart
+  createOrder
 }
