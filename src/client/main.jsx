c730d2aed4d944748b18940645e7ba2dab713d08
AzuraLulamoon <gabrielreffnercr@gmail.com>
2023-08-25 11:30:46 -0500

    adding MUI

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    primary: {main: "#4B3C2A"},
+    secondary: {main: "#3F8F29"}
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ThemeProvider theme={theme}>
       <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
