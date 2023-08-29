133d534e3b45bdf5f028d9e17f609d0a2a2eedd0
Benjamin McMahon <parachutehappens@gmail.com>
2023-08-29 10:10:23 -0700

    some session id stuff

--- src/client/components/Cart.jsx
+++ src/client/components/Cart.jsx
@@ -14,11 +14,12 @@ export default function Cart({token}){
   const [ products, setProducts ] = useState([]);
   const [ totalPrice, setTotalPrice ] = useState(0);
   const navigate = useNavigate();
+  const userID = sessionStorage.getItem('userID')
 
   useEffect(()=>{
     const getUserCart = async () => {
       try{
-        const response = await fetch(`${API}/users/${id}/cart`);
+        const response = await fetch(`${API}/users/${userID}/cart`);
         const result = await response.json();
         setProductArray(result.cart)
       }catch(err){
@@ -35,35 +36,34 @@ export default function Cart({token}){
       try{
         let workingTotal = 0
         const productDetailsArray = []
-        if(productArray.length > 0){
-          for(let i = 0; i < productArray.length; i++){
-            const response = await fetch(`${API}/products/${productArray[i].productId}`)
-            const result = await response.json()
-            productDetailsArray.push(result.singleProduct)
-            workingTotal += parseFloat(result.singleProduct.price)
-          }
-          setProducts(productDetailsArray)
-          setTotalPrice(workingTotal)
-        }else{
-          return
+        for(let i = 0; i < productArray.length; i++){
+          const response = await fetch(`${API}/products/${productArray[i].productId}`)
+          const result = await response.json()
+          productDetailsArray.push(result.singleProduct)
+          workingTotal += parseFloat(result.singleProduct.price)
         }
+        setProducts(productDetailsArray)
+        setTotalPrice(workingTotal)
       }catch(err){
         console.log('Error getting product items', err)
       }
     }
-    getCartProducts();
+    if (productArray === undefined || productArray.length <= 0){
+      return
+    }else{
+      getCartProducts();
+    }
   }, [productArray])
 
   async function checkout(){
     try{
-      console.log('Thanks for shopping with us!')
       const response = await fetch(`${API}/users/${id}/cart`, {
         method: "PATCH",
         headers: {"Content-Type" : "application/json"}
       })
       const result = await response.json()
-      console.alert('Thank you for shopping with us!')
-      console.log(result)
+      window.location.reload()
+      alert('Thank you for shopping with us!')
     }catch(err){
       console.log('Error checking out cart', err)
     }
@@ -77,7 +77,6 @@ export default function Cart({token}){
         <Typography sx={{textAlign:'center'}} variant='h5'>***Temporary, use params to naviagte between carts for ease of testing***</Typography>
         <Button onClick={()=>{
           checkout()
-          window.location.reload()
         }}>Proceed to Checkout</Button>
         {productArray ? (
         // runs when cart has products
